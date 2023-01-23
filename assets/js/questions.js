b097e55cf74d0cc62e3765b480071da0388672e4
psong1 <paulsong29@gmail.com>
2023-01-23 00:26:50 -0500

    fixing js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,13 +1,19 @@
-let questionArr = [
+let questions = [
     {
         title: 'How many day are there in a year?',
         choices: ['24', '365', '52', '12'],
-        correctAnswer: '365'
+        answer: '365'
     },
 
     {
         title: 'What is the capital of Georgia?',
         choices: ['Atlanta', 'Macon', 'Miami', 'Seattle'],
-        correctAnswer: 'Atlanta'
+        answer: 'Atlanta'
     },
+
+    {
+        title: 'test question',
+        choices: ['a', 'b', 'c', 'd'],
+        answer: 'a' 
+    }
 ]
