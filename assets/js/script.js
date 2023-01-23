3032e0bc6e8f4b4981711c6be2c1f5425a6b307f
psong1 <paulsong29@gmail.com>
2023-01-23 00:32:15 -0500

    fixing js

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,6 +104,7 @@ function quizEnd() {
   // show final score
   var finalScoreEl = document.getElementById("final-score");
   finalScoreEl.textContent = time;
+  // change time to correct answers
 
   // hide questions section
   questionsEl.setAttribute("class", "hide");
@@ -158,3 +159,33 @@ submitBtn.onclick = saveHighscore;
 startBtn.onclick = startQuiz;
 
 initialsEl.onkeyup = checkForEnter;
+
+function printHighscores() {
+    // either get scores from localstorage or set to empty array
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+  
+    // sort highscores by score property in descending order
+    highscores.sort(function(a, b) {
+      return b.score - a.score;
+    });
+  
+    highscores.forEach(function(score) {
+      // create li tag for each high score
+      var liTag = document.createElement("li");
+      liTag.textContent = score.initials + " - " + score.score;
+  
+      // display on page
+      var olEl = document.getElementById("highscores");
+      olEl.appendChild(liTag);
+    });
+  }
+  
+  function clearHighscores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  }
+  
+  document.getElementById("clear").onclick = clearHighscores;
+  
+  // run function when page loads
+  printHighscores();
